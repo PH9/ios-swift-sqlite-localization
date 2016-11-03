5e29ad6b38eee88870dd0fb1a8d801be04e24fb2
Wasith Theerapattrathamrong <phaicpe@gmail.com>
2016-11-03 17:02:49 +0700

    pod lib init

---
+++ Example/Pods/Target Support Files/Pods-SqliteLocalization_Example/Pods-SqliteLocalization_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SqliteLocalization_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SqliteLocalization_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-SqliteLocalization_Tests/Pods-SqliteLocalization_Tests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SqliteLocalization_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SqliteLocalization_TestsVersionString[];
+
