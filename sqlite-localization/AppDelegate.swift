06b2447a9cb2d5222ef2995b917c513bd5293dbe
Wasith Theerapattrathamrong <phaicpe@gmail.com>
2016-11-03 15:37:37 +0700

    Init Localization.shared in AppDelegate.swift

--- sqlite-localization/AppDelegate.swift
+++ sqlite-localization/AppDelegate.swift
@@ -12,5 +12,11 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        let sqlitePath = NSBundle.mainBundle().pathForResource("sample.sqlite", ofType: nil)!
+        Localization.shared = Localization(sqlitePath: sqlitePath, language: "th")
+        return true
+    }
 }
 
