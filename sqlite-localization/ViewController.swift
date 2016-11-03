516030498761ca0403bdf947cff5d148dcafa90a
Wasith Theerapattrathamrong <phaicpe@gmail.com>
2016-11-03 14:51:18 +0700

    Initial Commit

---
+++ sqlite-localization/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sqlite-localization
+//
+//  Created by Wasith Theerapattrathamrong on 11/3/2559 BE.
+//  Copyright © 2559 Wasith Theerapattrathamrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
